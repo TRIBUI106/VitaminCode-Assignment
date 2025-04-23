3047429168b56dc68744e00b31321d001b6538f4
chez1s <toptrainghieml@gmail.com>
2025-04-23 08:54:43 +0700

    Update Loan.java

--- src/main/java/org/banking/model/Loan.java
+++ src/main/java/org/banking/model/Loan.java
@@ -27,4 +27,68 @@ public class Loan {
         this.start_date = start_date;
         this.status = status;
     }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getAccount_id() {
+        return account_id;
+    }
+
+    public void setAccount_id(int account_id) {
+        this.account_id = account_id;
+    }
+
+    public int getLoad_package_id() {
+        return load_package_id;
+    }
+
+    public void setLoad_package_id(int load_package_id) {
+        this.load_package_id = load_package_id;
+    }
+
+    public BigDecimal getAmout() {
+        return amout;
+    }
+
+    public void setAmout(BigDecimal amout) {
+        this.amout = amout;
+    }
+
+    public BigDecimal getInterest_rate() {
+        return interest_rate;
+    }
+
+    public void setInterest_rate(BigDecimal interest_rate) {
+        this.interest_rate = interest_rate;
+    }
+
+    public int getTerm_in_months() {
+        return term_in_months;
+    }
+
+    public void setTerm_in_months(int term_in_months) {
+        this.term_in_months = term_in_months;
+    }
+
+    public Date getStart_date() {
+        return start_date;
+    }
+
+    public void setStart_date(Date start_date) {
+        this.start_date = start_date;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
 }
