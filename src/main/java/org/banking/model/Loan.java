7295d8eb6dee8413bde778cf1725c937b4c23a44
chez1s <toptrainghieml@gmail.com>
2025-04-23 08:55:15 +0700

    Update Loan.java

--- src/main/java/org/banking/model/Loan.java
+++ src/main/java/org/banking/model/Loan.java
@@ -16,4 +16,15 @@ public class Loan {
 
     public Loan() {
     }
+
+    public Loan(int id, int account_id, int load_package_id, BigDecimal amount, BigDecimal interest_rate, int term_in_months, Date start_date, String status) {
+        this.id = id;
+        this.account_id = account_id;
+        this.load_package_id = load_package_id;
+        this.amount = amount;
+        this.interest_rate = interest_rate;
+        this.term_in_months = term_in_months;
+        this.start_date = start_date;
+        this.status = status;
+    }
 }
